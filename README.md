8bb83fd3fbe650554a77d5d4d5a51604579bd3e1
wit <1229910+0xc22b@users.noreply.github.com>
2025-11-26 16:03:30 +0700

    Bump Safari version to 0.1.0

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # brace-extensions
+
+## Safari web extension
+
+xcrun safari-web-extension-packager safari --project-location safari --bundle-identifier com.bracedotto.brace.web-extension --swift --macos-only
---
+++ safari/Brace.to for Safari/Brace.to for Safari/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Brace.to for Safari
+//
+//  Created by Mac on 20/11/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
